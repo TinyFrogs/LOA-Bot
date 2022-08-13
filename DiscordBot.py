e66e230239347d25c2061e7d8b782f83ca3daef6
spiky-haired-Pyramid-head <87762815+spiky-haired-Pyramid-head@users.noreply.github.com>
2022-08-13 23:02:36 +0900

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,5 +1,6 @@
 import asyncio, discord
 import math
+import os
 
 from exam import *
 from discord.ext import commands
@@ -124,5 +125,5 @@ async def on_command_error(ctx, error):
         await ctx.send("명령어 몰?루")
         await ctx.send("https://tenor.com/view/%EB%AA%B0%EB%A3%A8-gif-23066933")
 
-
+access_token = os.environ["BOT_TOKEN"]
 bot.run('MTAwMzQ2MDM2NzM2NTcwMTY3Mw.Gc_p7O.Fwrx3sA9TS3e7oLaOlad9vIT7XjJlAkwWLeLMY')
