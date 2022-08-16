934908f3126ccf1b7b518c40fd08e33934dead75
spiky-haired-Pyramid-head <87762815+spiky-haired-Pyramid-head@users.noreply.github.com>
2022-08-17 02:27:51 +0900

    기존 코드 변경

--- bot.py
+++ bot.py
@@ -1,30 +1,25 @@
-import asyncio, discord
+import asyncio
 import math
-import os
 
-from exam import *
+import discord
 from discord.ext import commands
+from discord.utils import get
 
-bot = commands.Bot(command_prefix=".")
+bot = commands.Bot(command_prefix="$")
 
 
 @bot.event
 async def on_ready():
     print("We have loggedd in as {0.user}".format(bot))
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(".명령어(회랑, 회랑1)"))
-
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("$명령어"))
 
 @bot.command()
 async def 시발(ctx):
     await ctx.send("욕하지마 시발 :thumbsup:")
 
-
-
-
-
 #하드 + 헬
 @bot.command()
-async def 회랑1(ctx, num1, num2):
+async def 회랑헬(ctx, num1, num2):
     jewel = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     jewels = list(map(int, jewel))
 
@@ -87,43 +82,61 @@ async def 회랑(ctx, num1):
     await ctx.send(embed=embeded)
 
 @bot.command()
-async def test123(ctx):
-    print("테스트")
-    # result, _color, bot1, user = dice()
-    result = "틀렸습니다."
-    _color = 0xFF0000
-    bot1 = str(1)
-    user = str(2)
-    print("테스트1")
-    embed = discord.Embed(title="테스트", description="test embed", colour=_color)
-    print("테스트2")
-    embed.add_field(name="자신의 숫자", value=":book:" + user, inline=True)
-    print("테스트3")
-    embed.add_field(name="Bot의 숫자", value=":book:" + bot1, inline=True)
-    embed.add_field(name=None, value=":regional_indicator_s::regional_indicator_e::regional_indicator_x:", inline=True)
-    print("테스트4")
-    embed.set_footer(text="결과: " + result)
-    print("테스트5")
-    await ctx.send(embed=embed)
-    print("test end")
-
-
-@bot.command()
 async def 쿠크(ctx):
     embed = discord.Embed(title="쿠크 파티", description="쿠크 파티", colour=0x008080)
-    embed.add_field(name="각두", value="기상", inline=True)
-    embed.add_field(name="워로", value="홀나", inline=True)
-    embed.add_field(name="홀카", value="인파", inline=True)
-    embed.add_field(name="카멜", value="워로", inline=True)
+    embed.add_field(name="각각두", value="기상 | 알카 | 소서 | 아가", inline=True)
+    embed.add_field(name="워로드", value="홀나 | 기상 | 건슬 | 워로", inline=False)
+    embed.add_field(name="홀홀카", value="인파 | 아가 | 블레 | 리퍼", inline=False)
+    embed.add_field(name="카카멜", value="워로 | 충동 | 아가 | 배마", inline=False)
 
-    await ctx.send(embed=embeded)
+    await ctx.send(embed=embed)
 
+@bot.command()
+async def 배위(ctx):
+    await ctx.send("https://media.discordapp.net/attachments/865698548723744769/970330313257537566/802f117f8d9278ac.gif")
+
+@bot.command()
+async def 누가울어(ctx):
+    await ctx.send("https://coinpan.com/files/attach/images/198/700/600/175/7a21079132edda8c7575550e5d7fbf01.gif")
+
+@bot.command()
+async def 경매(ctx, num1):
+    party_gold_4 = int(num1) * 0.95 * 3 / 4
+    gold_4 = party_gold_4 - party_gold_4 * 0.1
+
+    party_gold_8 = int(num1) * 0.95 * 7 / 8
+    gold_8 = party_gold_8 - party_gold_8 * 0.1
+
+    embed = discord.Embed(title="경매 계산기", description="가격 약간 다름", colour=0x008080)
+    embed.add_field(name="인원수", value=4, inline=True)
+    embed.add_field(name="선점 입찰가", value=int(gold_4), inline=True)
+    embed.add_field(name="N빵 입찰가", value=int(party_gold_4), inline=True)
+    embed.add_field(name="인원수", value=8, inline=True)
+    embed.add_field(name="선점 입찰가", value=int(gold_8), inline=True)
+    embed.add_field(name="N빵 입찰가", value=int(party_gold_8), inline=True)
+
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def 명령어(ctx):
+    embed = discord.Embed(title="명령어", description=None, colour=0x008080)
+    embed.add_field(name="회랑 [노말/하드]", value="회랑 개수에 대한 보석 나옴", inline=True)
+    embed.add_field(name="회랑헬 [노말/하드] [헬]", value="노말 + 하드 + 헬 보석 개수 나옴", inline=False)
+    embed.add_field(name="경매 [골드]", value="4, 8인 기준 선점·N빵 입찰가 나옴", inline=False)
+    embed.add_field(name="쿠크", value="쿠크 파티 나옴", inline=False)
+    embed.add_field(name="배위", value="멋진 제리콘", inline=False)
+    embed.add_field(name="누가울어", value="임춘식", inline=False)
+
+    dm_channel = await ctx.message.author.create_dm()
+
+    await dm_channel.send(embed=embed)
+    await dm_channel.send("https://media.discordapp.net/attachments/865698548723744769/970330313257537566/802f117f8d9278ac.gif")
 
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-        await ctx.send("명령어 몰?루")
+        await ctx.send(f"{ctx.message.author.mention}명령어 몰?루")
         await ctx.send("https://tenor.com/view/%EB%AA%B0%EB%A3%A8-gif-23066933")
 
-access_token = os.environ['BOT_TOKEN']
-bot.run(access_token)
+
+bot.run('MTAwMzQ2MDM2NzM2NTcwMTY3Mw.Gc_p7O.Fwrx3sA9TS3e7oLaOlad9vIT7XjJlAkwWLeLMY')
